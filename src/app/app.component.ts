6f346d550f1002c8446e0f953c76746913e5a8ef
octaviobissutti <octavio_bissutti@hotmail.com>
2022-01-24 10:18:44 -0300

    First project

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,10 @@
-import { Component } from '@angular/core';
+import { Component } from '@angular/core'; //Nuestro componente
 
-@Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+@Component({ //Recibe un objeto con las siguientes propiedades
+  selector: 'app-root', //Le vamos a indicar que seleccione el app-root definido en el index.html. Sería como el root en react.
+  templateUrl: './app.component.html', //Definimos la ruta donde está ese selector para nuestro html.
+  styleUrls: ['./app.component.css'] //Lo mismo hacemos con la ruta de los estilos
 })
 export class AppComponent {
-  title = 'bases';
+ 
 }
